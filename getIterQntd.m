cda6c2f9157e94aa68353cf887193c7595d7e017
Bruno <brunoclaudinomatias@gmail.com>
2021-11-02 16:57:24 -0300

    Arrumando as bagunças

--- getIterQntd.m
+++
@@ -1,9 +0,0 @@
-function iter = getIterQntd(audioPath, maxSampleQntd)
-    info = audioinfo(audioPath);
-    if rem(info.TotalSamples, maxSampleQntd) == 0
-        iter = info.TotalSamples/maxSampleQntd;
-    else
-        aux = rem(info.TotalSamples, maxSampleQntd);
-        iter = ((info.TotalSamples-aux)/maxSampleQntd) + 1;
-    end
-end
--- myLMS.m
+++ myLMS.m
@@ -1,30 +1,25 @@
-function [e, y, w, u] = myLMS(dn, un, mu, M, w)
+function [e, y, w, xx] = myLMSFelipe(d, x, mu, M, w1)
 
-    %% Definição do número de iterações que serão executadas no loop
-    iter = length(dn);
-    
-    %% Validação dos parâmetros recebidos
-    if (iter <= M)  
-        error('Não é possível processar um sinal com a quantidade de amostras menor que a ordem do filtro utilizado.');
-    end
-    if (iter ~= length(un))  
-        error('Não é possível processar um sinal utilizando uma entrada de referência ' ...
-            + 'com uma quantidade diferente de amostras.'); 
-    end
-    
-    %% Inicialização dos vetores que serão utilizados no cálculo do LMS
-    y = zeros(length(dn),1);
-    e = zeros(length(dn),1);
-    u = zeros(length(dn), 1);
-    
-    %% Laço de repetição para a filtragem adaptativa
-    for n = 1:iter
-        u = [u(2:M);un(n)];
-        y(n) = w' * u;
-        e(n) = dn(n) - y(n);
-        if n >= M
-            w = w + mu * e(n) * u;
-        end
-    end
+Ns = length(d);
+if (Ns <= M)  
+    print('error');
+    return; 
+end
+if (Ns ~= length(x))  
+    print('error');
+    return; 
+end
+
+y = zeros(Ns,1);
+e = zeros(Ns,1);
+xx = zeros(Ns, 1);
+
+for n = 1:Ns
+    xx = [xx(2:M);x(n)];
+    y(n) = w1' * xx;
+    e(n) = d(n) - y(n);
+    w1 = w1 + mu * e(n) * xx;
+    w = w1;
+end
 
 end
